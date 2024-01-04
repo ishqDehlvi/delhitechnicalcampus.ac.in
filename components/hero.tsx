3142e9d13f31c531c8546b65c3db99fb7536d8af
ishqdehlvi <inzemamhaq05@gmail.com>
2024-01-05 01:29:15 +0530

    some updates using neovim

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,16 @@
-import * as React from "react";
+import * as React from "react"
+
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel";
+} from "@/components/ui/carousel"
 
 export function Hero() {
   const imageUrl =
-    "https://images.unsplash.com/photo-1574169207511-e21a21c8075a?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    "https://images.unsplash.com/photo-1574169207511-e21a21c8075a?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
   return (
     <div className=" grid grid-cols-2">
@@ -19,10 +20,16 @@ export function Hero() {
             <CarouselItem key={index}>
               <div className="grid grid-cols-2 h-full">
                 <div className="col-span-1 flex flex-col items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                  <p className="text-lg mt-4">Your carousel text goes here.</p>
+                  <span className="text-4xl font-semibold">
+                    Delhi Technical Campus
+                  </span>
+                  <p className="text-lg mt-4">
+                    Here you'll get all the updates whether it's related to
+                    academics, time tables or the notes and Collese societies
+                    announcements. You will get all the updates here....
+                  </p>
                   <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md">
-                    Connect With us
+                    Join Us..
                   </button>
                 </div>
                 <div className="col-span-1 h-full overflow-hidden rounded-full">
@@ -40,5 +47,5 @@ export function Hero() {
         <CarouselNext />
       </Carousel>
     </div>
-  );
+  )
 }
